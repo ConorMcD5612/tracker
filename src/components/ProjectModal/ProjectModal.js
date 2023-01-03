9432fc5408f2379e8593cd0f4b43f9a9b3b82c08
ConorMcD5612 <conordmcdevitt@gmail.com>
2023-01-02 20:29:55 -0500

    fixed ok button

--- src/components/ProjectModal/ProjectModal.js
+++ src/components/ProjectModal/ProjectModal.js
@@ -1,13 +1,14 @@
-  import React from 'react'
+import React from 'react'
 
 
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router'
-import {Tasks} from './Tasks'
+import { Tasks } from './Tasks'
+import { Link } from 'react-router-dom'
 
 
 export const ProjectModal = ({ closeModal, ...props }) => {
- 
+
   const [tasks, setTasks] = useState([])
 
   const params = useParams()
@@ -15,13 +16,13 @@ export const ProjectModal = ({ closeModal, ...props }) => {
   useEffect(() => {
 
     fetch(`http://localhost:5000/projects/${params.id}`)
-    .then((response) => response.json())
-    .then((data) => {
-     setTasks(data.tasks)
-    })
+      .then((response) => response.json())
+      .then((data) => {
+        setTasks(data.tasks)
+      })
     console.log("fetched")
   }, [tasks.length])
-  
+
 
   return (
     <div>
@@ -31,8 +32,11 @@ export const ProjectModal = ({ closeModal, ...props }) => {
       <h2>Total: 500 hrs </h2>
 
       <h1>Tasks</h1>
-      <Tasks tasks={tasks} setTasks={setTasks}/>
-      <button className='btn btn-primary' onClick={closeModal}>OK</button>
+      <Tasks tasks={tasks} setTasks={setTasks} />
+      <Link to="/projects">
+        <button className='btn btn-primary'>OK</button>
+      </Link>
+
     </div>
   )
 }
