c15c73566abadf9df46126fc8c34ee63ada222c0
ConorMcD5612 <conordmcdevitt@gmail.com>
2023-01-06 16:21:30 -0500

    changes

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,7 +7,7 @@ export const Project = (props) => {
             <div className="list-group-item list-group-item-action">
                 <div className='d-flex w-100 justify-content-between'>
                     <h4>{props.name}</h4>
-                    <h4> {props.hours}</h4>\
+                    <h4> {props.hours}</h4>
                     <Link to={props.name} >
                     <button className="btn">
                         <i class="bi bi-gear-fill"></i>
