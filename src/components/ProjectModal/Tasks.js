d1762091d3dccd0ea79e7073660fb4a27ebc927e
ConorMcD5612 <conordmcdevitt@gmail.com>
2023-01-07 19:45:56 -0500

    made so you have to complete subtasks first

--- src/components/ProjectModal/Tasks.js
+++ src/components/ProjectModal/Tasks.js
@@ -6,7 +6,10 @@ import { CompleteBtn } from './CompleteBtn'
 
 export const Tasks = ({ tasks, setTasks }) => {
   
-  
+  // const haveComplete = () => {
+  //   if()
+  // }
+
  
   return (
     <>
@@ -19,7 +22,9 @@ export const Tasks = ({ tasks, setTasks }) => {
               <h3>{task.description}</h3>
               <h3>{task.tier}</h3>
               <div>
-                <CompleteBtn task={task} tasks={tasks} setTasks={setTasks}/>
+                { index === tasks.length - 1 || tasks[index + 1].tier > task.tier ?  null : <CompleteBtn /> }
+               
+                {task.tier}
                 <AddTaskBtn type="sub"  task={task} index={index} setTasks={setTasks} tasks={tasks} />
                 <button className='btn'><i className="bi bi-chevron-down"></i></button>
               </div>
