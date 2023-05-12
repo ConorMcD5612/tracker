f4b3e78f580c03a6c02def8bbc04278adf7083a6
ConorMcD5612 <conordmcdevitt@gmail.com>
2023-05-12 14:29:29 -0400

    timer good? using library

--- src/components/ProjectModal/Timer.js
+++ src/components/ProjectModal/Timer.js
@@ -1,85 +1,47 @@
-import React from 'react'
-import { Pause, Check, Play } from 'react-feather'
-import { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react';
+import { useStopwatch } from "react-use-precision-timer";
+//https://www.npmjs.com/package/react-use-precision-timer?activeTab=readme
 
 export const Timer = () => {
-    const [timeLeft, setTimeLeft] = useState(60);
-    const [timerRunning, setTimerRunning] = useState(true);
-    const [showInput, setShowInput] = useState(false)
+  const stopwatch = useStopwatch();
+  const [[minutes, seconds], setMinSec] = useState([0, 0])
+  const [startSeconds, setStartSeconds] = useState(40*60)
 
-    useEffect(() => {
-        let interval;
-        //this should be when the timer starts? 
-        const startTime = Date.now()
-        if(timerRunning && timeLeft > 0){
-            interval = setInterval(() => {
-              setTimeLeft(Date.now() - )
-                //this doesn't update time 
-             
-            }, 1000)
-        }
-        
-        return () => clearInterval(interval)
-    }, [timerRunning, timeLeft])
+  const startTimer = () => {
+    stopwatch.start();
+    console.log("start timer")
+  }
 
-    function handlePause() {
-        setTimerRunning(!timerRunning);
-    }
+  const pauseHandler = () => {
+    stopwatch.isPaused() ? stopwatch.resume() : stopwatch.pause()
+    console.log(stopwatch.isPaused)
+  }
 
+  const unixToMinSec = () => {
 
-    function handleReset() {    
-        setTimeLeft(60);
-        setTimerRunning(true);
-    }
+    const totalSeconds = Math.ceil(startSeconds - (stopwatch.getElapsedRunningTime() / 1000))
+    const seconds = Math.floor(totalSeconds % 60)
+    const minutes = Math.floor(totalSeconds / 60)
+   
+    return {minutes, seconds}
+  }
 
+  useEffect(() => {
+   const interval = setInterval(() => {
+    const {minutes, seconds} = unixToMinSec();
+    setMinSec([minutes, seconds])
+    console.log(minutes, seconds)
+   }, 1000)
 
-    ///these two functions are going to be two seperate componenets
-    const blurHandler = () => {
-        // check input regex 00:00 format 
-        // const regex = /[0-9][0-9]:[0-9][0-9]/
-        // if (regex.test(timer)) {
-        //     setShowInput(false)
-        // } else {
-        //     console.log("invalid input for timer format is : 00:00")
-        // }
-    }
+  return () => clearInterval(interval)
+  }, [])
 
-    const changeTimer = (e) => {
-        // let seconds = Number(e.target.value.slice(3))
-        // let minutes = Number(e.target.value.slice(0, 2))
-        // let newTime = (minutes * 60) + seconds
-
-        // setTimer(e.target.value)
-        // setAddTime(newTime)
-    }
-
-    return (
-
-        <div className='timer-container'>
-            <div className='timer-flex'>
-                {showInput ? (
-                    <input autoFocus type="text" onBlur={blurHandler} onChange={(e) => changeTimer(e)} name="description" />
-                ) : (
-                    <div onDoubleClick={() => setShowInput(true)} className='timer'>
-                        <h2>{timeLeft}</h2>
-                    </div>
-                )}
-                <div className='timerbtn-flex'>
-
-                    {setTimerRunning ? (
-                        <button className='pause-btn' onClick={handlePause}>
-                            <Play />
-                        </button>
-                    ) : (
-                        <button onClick={handlePause} className='pause-btn'>
-                            <Pause />
-                        </button>
-                    )}
-
-                    <button className='done-btn' onClick={handleReset}><Check /></button>
-                </div>
-            </div>
-        </div >
-
-    )
-}
+ 
+  return (
+    <div>
+     <button onClick={() => startTimer()}>START BUTTON </button>
+     <div>{`${minutes}:${seconds < 10 ? '0' : ''}${seconds}`}</div>
+     <button onClick={() => pauseHandler()}>PAUSE BUTTON</button>
+    </div>
+  );
+};
