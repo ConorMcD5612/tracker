8cf7b450634b7664a911aa59ec4dc6907bc938a9
Conor <conordmcdevitt@gmail.com>
2023-08-07 21:23:38 -0400

    do I need to hide that key idk if it matters

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
 import { AddProject } from "./components/ProjectSection/AddProject";
 import { ProjectModal } from "./components/ProjectModal/ProjectModal"
 import { Timer } from "./components/ProjectModal/Timer"
+import { useEffect } from 'react';
+import jwt_decode from "jwt-decode0"
 
 
 
@@ -14,9 +16,31 @@ function App() {
   const location = useLocation();
   const background = location.state && location.state.background;
 
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT iD token: " + response.credential )
+    let userObject = jwt_decode(response.credential)
+  }
+
+  useEffect(() => {
+    
+    window.google.accounts.id.initialize({
+      client_id: "435547155571-g6e0c4uae4kp7d8m0r55g544fokecfof.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    })
+
+    window.google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "outline", size: "large"}
+    )
+
+  }, [])
+
   return (
 
     <div>
+      <div id="signInDiv">
+
+      </div>
      
         <Routes>
           <Route path="projects" element={<ProjectSection />} />
