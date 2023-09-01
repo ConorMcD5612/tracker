00a4094b48173daaeb1dbee1cb465526bfdc9563
Conor <conordmcdevitt@gmail.com>
2023-09-01 19:22:02 -0400

    vercel.json cors headers

--- server/server.js
+++ server/server.js
@@ -1,24 +1,4 @@
-// const express = require("express");
-// const app = express();
-// const cors = require("cors")
-// require("dotenv").config({path: "./env"});
-// const port = process.env.PORT || 5000;
 
-
-// app.use(cors())
-// app.use(express.json())
-// app.use(require("./routes/records"))
-
-// //driver connection?
-// const dbo = require("./db/conn");
-
-// app.listen(port, () => {
-//     dbo.connectToServer(function (err) {
-//         if(err) console.err(err)
-//     });
-
-//     console.log(`Server is running on ${port}`)
-// })
 const express = require("express");
 const app = express();
 const cors = require("cors");
