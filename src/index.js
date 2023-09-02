5c981712775ad4049bd27707c8db9f9bf64e6042
Conor <conordmcdevitt@gmail.com>
2023-09-01 20:15:57 -0400

    basename

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router >
+    <Router  basename={process.env.PUBLIC_URL}>
       <App />
     </Router>
   </React.StrictMode>
@@ -21,4 +21,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-  
