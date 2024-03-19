9243c3ac4c1ed3dff46917b85b900727b491037f
Conor <conordmcdevitt@gmail.com>
2024-03-19 14:40:57 -0400

    api folder

--- server/api/index.js
+++ server/api/index.js
@@ -14,9 +14,9 @@ app.use(cors(
 ));
 
 app.use(express.json());
-app.use(require("./routes/record"));
+app.use(require("../routes/record"));
 // get driver connection
-const dbo = require("./db/conn");
+const dbo = require("../db/conn");
 
 app.get("/", (req, res) => {
   res.json("WORK")
